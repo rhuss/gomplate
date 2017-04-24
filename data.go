f5ed8ab4429c505f734ba5764b238007dd50b04d
Roland Huß <roland@ro14nd.de>
2017-04-24 09:54:47 +0200

    Add support for datasource directories
    When the argument given with `--datasource` point to a directory, every file in this directory is processed with the plain-file logic for datasource (e.g. name of ds == filebasename, type determined by file extension).
    
    Related to #117

--- data.go
+++ data.go
@@ -69,13 +69,15 @@ func NewData(datasourceArgs []string, headerArgs []string) *Data {
 	sources := make(map[string]*Source)
 	headers := parseHeaderArgs(headerArgs)
 	for _, v := range datasourceArgs {
-		s, err := ParseSource(v)
-		if err != nil {
-			log.Fatalf("error parsing datasource %v", err)
-			return nil
+		for _, arg := range fanOutDatasourceArgs(v) {
+			s, err := ParseSource(arg)
+			if err != nil {
+				log.Fatalf("error parsing datasource %s: %v", arg, err)
+				return nil
+			}
+			s.Header = headers[s.Alias]
+			sources[s.Alias] = s
 		}
-		s.Header = headers[s.Alias]
-		sources[s.Alias] = s
 	}
 	return &Data{
 		Sources:  sources,
@@ -132,13 +134,13 @@ func ParseSource(value string) (*Source, error) {
 	)
 	parts := strings.SplitN(value, "=", 2)
 	if len(parts) == 1 {
-		f := parts[0]
-		alias = strings.SplitN(value, ".", 2)[0]
-		if path.Base(f) != f {
-			err := fmt.Errorf("Invalid datasource (%s). Must provide an alias with files not in working directory", value)
+		srcURL = absURL(value)
+		if srcURL.Scheme == "file" {
+			alias = strings.SplitN(path.Base(value), ".", 2)[0]
+		} else {
+			err := fmt.Errorf("Invalid datasource (%s). Must provide an alias name for non-file datasources", value)
 			return nil, err
 		}
-		srcURL = absURL(f)
 	} else if len(parts) == 2 {
 		alias = parts[0]
 		var err error
@@ -157,6 +159,14 @@ func ParseSource(value string) (*Source, error) {
 }
 
 func absURL(value string) *url.URL {
+	parsedURL, err := url.Parse(value)
+	if err != nil || parsedURL.Scheme == "" || !parsedURL.IsAbs() {
+		return fileToURL(value)
+	}
+	return parsedURL
+}
+
+func fileToURL(path string) *url.URL {
 	cwd, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("Can't get working directory: %s", err)
@@ -166,7 +176,7 @@ func absURL(value string) *url.URL {
 		Path:   cwd + "/",
 	}
 	relURL := &url.URL{
-		Path: value,
+		Path: path,
 	}
 	return baseURL.ResolveReference(relURL)
 }
@@ -209,6 +219,28 @@ func (d *Data) dataSourceParallel(alias string, args ...string) map[string]inter
 	return b
 }
 
+// ReadParsedSource - thread safe reading of the source including parsing. Fully cached (including parsed content)
+func (d *Data) ReadParsedSource(fs vfs.Filesystem, source *Source, args ...string) (map[string]interface{}, error) {
+
+	key := calcCacheKey(source, args)
+
+	if cached, ok := d.checkParallelCache(key); ok {
+		return cached, nil
+	}
+
+	if r, ok := sourceReaders[source.URL.Scheme]; ok {
+		data, err := r(source, args...)
+		if err != nil {
+			return nil, err
+		}
+		structuredData := parseData(source, data)
+		d.addToParallelCache(key, structuredData)
+		return structuredData, nil
+	}
+	log.Fatalf("Datasources with scheme %s not yet supported", source.URL.Scheme)
+	return nil, nil
+}
+
 // ReadSource -
 func (d *Data) ReadSource(fs vfs.Filesystem, source *Source, args ...string) ([]byte, error) {
 	if d.plainCache == nil {
@@ -234,26 +266,24 @@ func (d *Data) ReadSource(fs vfs.Filesystem, source *Source, args ...string) ([]
 	return nil, nil
 }
 
-// ReadParsedSource - thread safe reading of the source including parsing. Fully cached (including parsed content)
-func (d *Data) ReadParsedSource(fs vfs.Filesystem, source *Source, args ...string) (map[string]interface{}, error) {
-
-	key := calcCacheKey(source, args)
-
-	if cached, ok := d.checkParallelCache(key); ok {
-		return cached, nil
-	}
-
-	if r, ok := sourceReaders[source.URL.Scheme]; ok {
-		data, err := r(source, args...)
+// If argument is a plain directory, use dir contents as datasource files
+func fanOutDatasourceArgs(arg string) []string {
+	if _, err := os.Stat(arg); err == nil {
+		files, err := ioutil.ReadDir(arg)
 		if err != nil {
-			return nil, err
+			log.Fatalf("error parsing datasource directory %s %v", arg, err)
+			return nil
 		}
-		structuredData := parseData(source, data)
-		d.addToParallelCache(key, structuredData)
-		return structuredData, nil
+
+		var paths []string
+		for _, file := range files {
+			if !file.IsDir() {
+				paths = append(paths, path.Join(arg, file.Name()))
+			}
+		}
+		return paths
 	}
-	log.Fatalf("Datasources with scheme %s not yet supported", source.URL.Scheme)
-	return nil, nil
+	return []string{arg}
 }
 
 func parseData(source *Source, data []byte) map[string]interface{} {
